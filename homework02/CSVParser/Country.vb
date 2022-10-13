c1340c6579b81da432fb2ebd95811380e6b3ca7d
rocco1414 <114834587+rocco1414@users.noreply.github.com>
2022-10-13 13:56:04 +0200

    Add files via upload

---
+++ homework02/CSVParser/Country.vb
@@ -0,0 +1,19 @@
+﻿
+Public Class Country
+        Public Country As String
+        Public Continent As String
+        Public DateOfCollection As String
+        Public GDP As Double
+        Public GNI As Double
+        Public LiveBirthsNumber As Integer
+        Public LiveBirthsRate As String
+        Public DeathsNumber As Integer
+        Public DeathRate As String
+        Public RateOfIncrease As Double
+        Public InfantDeathsNumber As Integer
+        Public InfantDeathsRatePer1000Births As Double
+        Public LifeExpectancyAtBirthMale As Double
+        Public LifeExpectancyAtBirthFemale As Double
+        Public TotalFertilityRate As Double
+    End Class
+
---
+++ homework02/CSVParser/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ButtonGetFile = New System.Windows.Forms.Button()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(114, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Read"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 53)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(776, 52)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'ButtonGetFile
+        '
+        Me.ButtonGetFile.Location = New System.Drawing.Point(21, 12)
+        Me.ButtonGetFile.Name = "ButtonGetFile"
+        Me.ButtonGetFile.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonGetFile.TabIndex = 2
+        Me.ButtonGetFile.Text = "GetFile"
+        Me.ButtonGetFile.UseVisualStyleBackColor = True
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(12, 132)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(776, 306)
+        Me.RichTextBox2.TabIndex = 3
+        Me.RichTextBox2.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(640, 360)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.ButtonGetFile)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ButtonGetFile As Button
+    Friend WithEvents RichTextBox2 As RichTextBox
+End Class
