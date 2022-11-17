fc56ca8fb8093201f39f154c20352aeb1a6ddcf4
rocco1414 <114834587+rocco1414@users.noreply.github.com>
2022-11-17 21:08:37 +0100

    Add files via upload

---
+++ homework05-histogram/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace homework05_histogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(23, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(765, 371);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1052, 550);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(120, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Vertical";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 629);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ homework05-histogram/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace homework05_histogram
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Graphics g;
+        public Bitmap b;
+        int[] list;
+        public Random r = new Random();
+
+        int width = 300;
+        int height = 400;
+
+        
+
+        private void initialize()
+        {
+            this.b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            this.g = Graphics.FromImage(b);
+        }
+
+
+        //Random distribution
+        private void generateDistribution()
+        {
+            Random rand = new Random();
+            list = new int[100];
+            for (int i = 0; i < 100; i++)
+                list[i] = rand.Next(1, 100);
+        }
+
+        //rectangle
+        private void createRectangle()
+        {
+            g.DrawRectangle(Pens.Black, 10, 10, width, height);
+            pictureBox1.Image = b;
+            pictureBox1.Refresh();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            initialize();
+        }
+
+        //private int FromXRealToXVirtual(int X, int minX, int maxX, int W)
+        //{
+        //    return W * (X - minX) / (maxX - minX);
+        //}
+
+        private int fromXRealToXVirtual(double x, double minX, double maxX, int left, int w)
+        {
+            return left + (int)(w * (x - minX) / (maxX - minX));
+        }
+
+        private int fromYRealToYVirtual(double y, double minY, double maxY, int top, int h)
+        {
+            return top + (int)(h - h * (y - minY) / (maxY - minY));
+        }
+
+        private void displayDistribution()
+        {
+            int x = 10;
+            int y = 10;
+            int max = list.Max();
+            int min = list.Min();
+            int range = max - min;
+
+            if (this.checkBox1.Checked)
+            { // Vertical
+                int barWidth = width / 100;
+                int barHeight = 0;
+                int scale = height / range;
+
+                for (int i = 0; i < 100; i++)
+                {
+                    barHeight = list[i] * scale;
+                    g.DrawRectangle(Pens.Black, x, y + height - barHeight, barWidth, barHeight);
+                    g.FillRectangle(new SolidBrush(Color.FromArgb(64, 0, 255, 0)), x, y + height - barHeight, barWidth, barHeight);
+                    x += barWidth;
+                }
+            }
+            else
+            { // Horizontal
+                int barHeight = height / 100;
+                int barWidth = 0;
+                int scale = width / range;
+
+                for (int i = 0; i < 100; i++)
+                {
+                    barWidth = list[i] * scale;
+                    g.DrawRectangle(Pens.Black, x, y, width - barWidth, barHeight);
+                    g.FillRectangle(new SolidBrush(Color.FromArgb(64, 0, 255, 0)), x, y, width - barWidth, barHeight);
+                    y += barHeight;
+                }
+            }
+
+            pictureBox1.Image = b;
+            pictureBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            generateDistribution();
+            createRectangle();
+            displayDistribution();
+        }
+    }
+}
