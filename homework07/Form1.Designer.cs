91c3f2de40885c73c716644df3fc07dac0fef712
rocco1414 <114834587+rocco1414@users.noreply.github.com>
2022-11-23 18:38:30 +0100

    Add files via upload

---
+++ homework07/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace homework07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.UpDownProbability = new System.Windows.Forms.NumericUpDown();
+            this.Compute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownProbability)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // UpDownProbability
+            // 
+            this.UpDownProbability.Location = new System.Drawing.Point(12, 34);
+            this.UpDownProbability.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.UpDownProbability.Name = "UpDownProbability";
+            this.UpDownProbability.Size = new System.Drawing.Size(120, 22);
+            this.UpDownProbability.TabIndex = 8;
+            this.UpDownProbability.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            
+            this.Compute.Location = new System.Drawing.Point(485, 33);
+            this.Compute.Name = "Compute";
+            this.Compute.Size = new System.Drawing.Size(84, 23);
+            this.Compute.TabIndex = 14;
+            this.Compute.Text = "Absolute";
+            this.Compute.UseVisualStyleBackColor = true;
+            this.Compute.Click += new System.EventHandler(this.Compute_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Lambda";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 75);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(409, 363);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(427, 75);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(361, 363);
+            this.richTextBox2.TabIndex = 19;
+            this.richTextBox2.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(409, 363);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(794, 75);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(279, 363);
+            this.richTextBox3.TabIndex = 22;
+            this.richTextBox3.Text = "";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(794, 75);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(279, 360);
+            this.pictureBox3.TabIndex = 23;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(427, 75);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(361, 363);
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(152, 34);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 25;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(297, 34);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(124, 22);
+            this.numericUpDown2.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Number of trajectory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Number of trials";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(596, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 23);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Relative";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Normalized";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 447);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Compute);
+            this.Controls.Add(this.UpDownProbability);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownProbability)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown UpDownProbability;
+        private System.Windows.Forms.Button Compute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ homework07/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace homework07
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap b;
+        public Graphics g;
+        public Bitmap b2;
+        public Graphics g2;
+        public Bitmap b3;
+        public Graphics g3;
+        public Random random = new Random();
+        public Pen PenTrajectory = new Pen(Color.Red, 2);
+        public Pen PenTrajectory2 = new Pen(Color.Black, 2);
+
+        private List<int> FrequOflastTry;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void vertical(Bitmap b, Graphics g, PictureBox pictureBox, Dictionary<int, int> distr, int numElement)
+        {
+            int j = 0;
+            int step1 = (int)(pictureBox.Width / (double)distr.Count);
+
+            g.DrawRectangle(PenTrajectory2, 0, 0, pictureBox.Width - 1, pictureBox.Height - 1);
+
+            foreach (KeyValuePair<int, int> pair in distr)
+            {
+                double virtualX = FromXRealToXVirtual(pair.Value, 0, numElement, 0, pictureBox.Height);
+                Rectangle r = new Rectangle(j + 1, System.Convert.ToInt32(pictureBox.Height - (virtualX) - 1), step1, (int)virtualX);
+                g.DrawRectangle(PenTrajectory2, j + 1, System.Convert.ToInt32(pictureBox.Height - (virtualX) - 1), step1, (int)virtualX);
+                Brush x2 = new SolidBrush(Color.Blue);
+                g.FillRectangle(x2, r);
+                j = j + step1;
+            }
+
+            pictureBox.Image = b;
+        }
+
+
+        public double FromXRealToXVirtual(double X, double minX, double maxX, int Left, int W)
+        {
+            if ((maxX - minX) == 0)
+                return 0;
+
+            return Left + W * (X - minX) / (maxX - minX);
+        }
+
+        public double FromYRealToYVirtual(double Y, double minY, double maxY, int Top, int H)
+        {
+            if ((maxY - minY) == 0)
+                return 0;
+
+            return Top + H - H * (Y - minY) / (maxY - minY);
+        }
+
+        private void Compute_Click(object sender, EventArgs e)
+        {
+            this.b = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            this.g = Graphics.FromImage(b);
+            this.g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g.Clear(Color.White);
+            this.b2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            this.g2 = Graphics.FromImage(b2);
+            this.g2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g2.Clear(Color.White);
+            this.b3 = new Bitmap(this.pictureBox3.Width, this.pictureBox3.Height);
+            this.g3 = Graphics.FromImage(b3);
+            this.g3.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g3.Clear(Color.White);
+
+            var interarrival = 0;
+            Dictionary<int, int> interrarivalDistribution = new Dictionary<int, int>();
+            int TrialsCount = (int)numericUpDown2.Value;
+            int NumerOfTrajectories = (int)numericUpDown1.Value;
+            double SuccessProbability = (double)UpDownProbability.Value / (double)TrialsCount;
+
+            double minX = 0;
+            double maxX = TrialsCount;
+            double minY = 0;
+            double maxY = TrialsCount;
+
+
+            Rectangle VirtualWindow = new Rectangle(20, 20, this.b.Width - 40, this.b.Height - 40);
+
+            g.DrawRectangle(Pens.DarkSlateGray, VirtualWindow);
+
+            int[] altezze = new int[this.pictureBox1.Height + 1];
+            for (int i = 0; i <= this.pictureBox1.Height; i++)
+                altezze[i] = 0;
+
+            for (int i = 1; i <= NumerOfTrajectories; i++)
+            {
+                List<Point> Punti = new List<Point>();
+                double Y = 0;
+                for (int X = 1; X <= TrialsCount; X++)
+                {
+                    double Uniform = random.NextDouble();
+                    if (Uniform < SuccessProbability)
+                    {
+                        Y = Y + 1;
+                        if (interarrival != 0)
+                        {
+                            if (!interrarivalDistribution.ContainsKey(interarrival))
+                                interrarivalDistribution.Add(interarrival, 1);
+                            else
+                                interrarivalDistribution[interarrival] = interrarivalDistribution[interarrival] + 1;
+                            interarrival = 0;
+                        }
+                    }
+                    else
+                        interarrival = interarrival + 1;
+                    double xDevice = FromXRealToXVirtual(X, minX, maxX, VirtualWindow.Left, VirtualWindow.Width);
+                    double YDevice = FromYRealToYVirtual(Y, minY, maxY, VirtualWindow.Top, VirtualWindow.Height);
+                    Punti.Add(new Point((int)xDevice, (int)YDevice));
+                }
+                g.DrawLines(PenTrajectory, Punti.ToArray());
+                altezze[Punti.Last().Y] += 100;
+            }
+
+            Pen pen = new Pen(Color.Black, 5);
+            this.pictureBox1.Image = b;
+            this.pictureBox2.Image = b2;
+
+            for (int i = 0; i <= altezze.Length - 1; i++)
+
+                g2.DrawLine(pen, 0, i, altezze[i], i);
+
+
+            vertical(b3, g3, pictureBox3, interrarivalDistribution, TrialsCount);
+            pictureBox3.Image = b3;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.b = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            this.b2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            this.g = Graphics.FromImage(b);
+            this.g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g.Clear(Color.White);
+            this.g2 = Graphics.FromImage(b2);
+            this.g2.Clear(Color.White);
+            this.g2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.b3 = new Bitmap(this.pictureBox3.Width, this.pictureBox3.Height);
+            this.g3 = Graphics.FromImage(b3);
+            this.g3.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g3.Clear(Color.White);
+
+            var interarrival = 0;
+            Dictionary<int, int> interrarivalDistribution = new Dictionary<int, int>();
+
+            int TrialsCount = (int)numericUpDown2.Value;
+            int NumerOfTrajectories = (int)numericUpDown1.Value;
+            double SuccessProbability = (double)UpDownProbability.Value / (double)TrialsCount;
+
+            double minX = 0;
+            double maxX = TrialsCount;
+            double minY = 0;
+            double maxY = TrialsCount;
+
+            Rectangle VirtualWindow = new Rectangle(20, 20, this.b.Width - 40, this.b.Height - 40);
+
+            int[] altezze = new int[this.pictureBox1.Height + 1];
+            for (int i = 0; i <= this.pictureBox1.Height; i++)
+                altezze[i] = 0;
+
+            g.DrawRectangle(Pens.DarkSlateGray, VirtualWindow);
+
+            for (int i = 1; i <= NumerOfTrajectories; i++)
+            {
+                List<Point> Punti = new List<Point>();
+                double Y = 0;
+                for (int X = 1; X <= TrialsCount; X++)
+                {
+                    double Uniform = random.NextDouble();
+                    if (Uniform < SuccessProbability)
+                    {
+                        Y = Y + 1;
+                        if (interarrival != 0)
+                        {
+                            if (!interrarivalDistribution.ContainsKey(interarrival))
+                                interrarivalDistribution.Add(interarrival, 1);
+                            else
+                                interrarivalDistribution[interarrival] = interrarivalDistribution[interarrival] + 1;
+                            interarrival = 0;
+                        }
+                    }
+                    else
+                        interarrival = interarrival + 1;
+                    double xDevice = FromXRealToXVirtual(X, minX, maxX, VirtualWindow.Left, VirtualWindow.Width);
+                    double YDevice = FromYRealToYVirtual(Y * TrialsCount / (X + 1), minY, maxY, VirtualWindow.Top, VirtualWindow.Height);
+                    Punti.Add(new Point((int)xDevice, (int)YDevice));
+                }
+                g.DrawLines(PenTrajectory, Punti.ToArray());
+                altezze[Punti.Last().Y] += 100;
+            }
+
+            Pen pen = new Pen(Color.Black, 5);
+            this.pictureBox1.Image = b;
+            this.pictureBox2.Image = b2;
+            for (int i = 0; i <= altezze.Length - 1; i++)
+
+                g2.DrawLine(pen, 0, i, altezze[i], i);
+
+            this.pictureBox1.Image = b;
+
+            vertical(b3, g3, pictureBox3, interrarivalDistribution, TrialsCount);
+            pictureBox3.Image = b3;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.b = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            this.b2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            this.g = Graphics.FromImage(b);
+            this.g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g.Clear(Color.White);
+            this.g2 = Graphics.FromImage(b2);
+            this.g2.Clear(Color.White);
+            this.g2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.b3 = new Bitmap(this.pictureBox3.Width, this.pictureBox3.Height);
+            this.g3 = Graphics.FromImage(b3);
+            this.g3.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g3.Clear(Color.White);
+
+            var interarrival = 0;
+            Dictionary<int, int> interrarivalDistribution = new Dictionary<int, int>();
+
+            int TrialsCount = (int)numericUpDown2.Value;
+            int NumerOfTrajectories = (int)numericUpDown1.Value;
+            double SuccessProbability = (double)UpDownProbability.Value / (double)TrialsCount;
+
+            double minX = 0;
+            double maxX = TrialsCount;
+            double minY = 0;
+            double maxY = TrialsCount;
+
+            Rectangle VirtualWindow = new Rectangle(20, 20, this.b.Width - 40, this.b.Height - 40);
+
+            int[] altezze = new int[this.pictureBox1.Height + 1];
+            for (int i = 0; i <= this.pictureBox1.Height; i++)
+                altezze[i] = 0;
+
+            g.DrawRectangle(Pens.DarkSlateGray, VirtualWindow);
+
+            for (int i = 1; i <= NumerOfTrajectories; i++)
+            {
+                List<Point> Punti = new List<Point>();
+                double Y = 0;
+                for (int X = 1; X <= TrialsCount; X++)
+                {
+                    double Uniform = random.NextDouble();
+                    if (Uniform < SuccessProbability)
+                    {
+                        Y = Y + 1;
+                        if (interarrival != 0)
+                        {
+                            if (!interrarivalDistribution.ContainsKey(interarrival))
+                                interrarivalDistribution.Add(interarrival, 1);
+                            else
+                                interrarivalDistribution[interarrival] = interrarivalDistribution[interarrival] + 1;
+                            interarrival = 0;
+                        }
+                    }
+                    else
+                        interarrival = interarrival + 1;
+                    double xDevice = FromXRealToXVirtual(X, minX, maxX, VirtualWindow.Left, VirtualWindow.Width);
+                    double YDevice = FromYRealToYVirtual(Y * Math.Sqrt(TrialsCount) / Math.Sqrt(X + 1), minY, maxY, VirtualWindow.Top, VirtualWindow.Height);
+                    Punti.Add(new Point((int)xDevice, (int)YDevice));
+                }
+                g.DrawLines(PenTrajectory, Punti.ToArray());
+                altezze[Punti.Last().Y] += 100;
+            }
+
+            Pen pen = new Pen(Color.Black, 5);
+            this.pictureBox1.Image = b;
+            this.pictureBox2.Image = b2;
+            for (int i = 0; i <= altezze.Length - 1; i++)
+
+                g2.DrawLine(pen, 0, i, altezze[i], i);
+
+            this.pictureBox1.Image = b;
+
+            vertical(b3, g3, pictureBox3, interrarivalDistribution, TrialsCount);
+            pictureBox3.Image = b3;
+        }
+    }
+}
