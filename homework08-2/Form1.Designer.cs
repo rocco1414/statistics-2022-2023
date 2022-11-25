d42b139b8a7640b409121c0028af87a2c63a5d6a
rocco1414 <114834587+rocco1414@users.noreply.github.com>
2022-11-25 09:43:14 +0100

    Add files via upload

---
+++ homework08-2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace homework08_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Normal distribution";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Chi-Squared distribution";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Cauchy distribution";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(477, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(248, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Fisher-Snedecor distribution";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(731, 20);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 24);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "T-Student distribution";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(924, 12);
+            this.trackBar1.Maximum = 10000;
+            this.trackBar1.Minimum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1034, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "100";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1061, 473);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1061, 473);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 558);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ homework08-2/Form1.cs
@@ -0,0 +1,440 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework08_2
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        Pen p = new Pen(Color.Green);
+        Bitmap b;
+        Graphics g;
+        int numberOftrials = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.setUpWindow();
+            double minValue = -3;
+            double maxValue = 3;
+            double delta = maxValue - minValue;
+            double numberOfInterval = 150;
+            double sizeOfInterval = delta / numberOfInterval;
+
+            Dictionary<double, int> histogramDict = new Dictionary<double, int>();
+            double tempValue = minValue;
+            for (int i = 0; i <= numberOfInterval; i++)
+            {
+                tempValue = minValue + (sizeOfInterval * i);
+                tempValue = Math.Round(tempValue, 2);
+                histogramDict[tempValue] = 0;
+            }
+
+            int total = 0;
+
+            for (int x = 0; x < numberOftrials; x++)
+            {
+                double xRnd = (r.NextDouble() * 2) - 1;
+                double value1 = 0;
+
+
+                double yRnd = (r.NextDouble() * 2) - 1;
+
+                double s = (xRnd * xRnd) + (yRnd * yRnd);
+
+                while (s < 0 || s > 1)
+                {
+                    xRnd = (r.NextDouble() * 2) - 1;
+                    yRnd = (r.NextDouble() * 2) - 1;
+                    s = (xRnd * xRnd) + (yRnd * yRnd);
+                }
+
+                xRnd = xRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+                yRnd = yRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+
+                value1 = xRnd;
+
+
+                foreach (double key in histogramDict.Keys)
+                {
+                    double range = key + sizeOfInterval;
+                    if (range > maxValue) range = maxValue;
+                    if (value1 < range && value1 > key)
+                    {
+                        histogramDict[key] += 1;
+                        if (total < histogramDict[key])
+                        {
+                            total = histogramDict[key];
+                        }
+                        break;
+                    }
+                }
+            }
+            g.TranslateTransform(0, this.b.Height);
+            g.ScaleTransform(1, -1);
+
+            int idIstogram = 0;
+            int widthIstogram = (int)(this.b.Width / numberOfInterval);
+            double lastKeyY = 0;
+
+            foreach (double key in histogramDict.Keys)
+            {
+                lastKeyY = key;
+                int newHeight = histogramDict[key] * this.b.Height / total;
+                int newX = (widthIstogram * idIstogram) + 10;
+                Rectangle isto = new Rectangle(newX, 0, widthIstogram, newHeight);
+                idIstogram++;
+
+                int nextWidthIstogram = (int)(widthIstogram * idIstogram * 1);
+
+                g.DrawRectangle(Pens.Green, isto);
+                g.FillRectangle(Brushes.Green, isto);
+            }
+            this.pictureBox1.Image = b;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.setUpWindow();
+
+            double minValue = 0;
+            double maxValue = 4;
+
+            double delta = maxValue - minValue;
+            double nintervals = 150;
+            double intervalsSize = delta / nintervals;
+
+            Dictionary<double, int> istogramDict = new Dictionary<double, int>();
+            double tempValue = minValue;
+            for (int i = 0; i <= nintervals; i++)
+            {
+                tempValue = minValue + (intervalsSize * i);
+                tempValue = Math.Round(tempValue, 2);
+                istogramDict[tempValue] = 0;
+            }
+
+            int total = 0;
+
+            for (int x = 0; x < this.numberOftrials; x++)
+            {
+                double xRnd = (r.NextDouble() * 2) - 1;
+                double value2 = 0;
+                double yRnd = (r.NextDouble() * 2) - 1;
+                double s = (xRnd * xRnd) + (yRnd * yRnd);
+
+                while (s < 0 || s > 1)
+                {
+                    xRnd = (r.NextDouble() * 2) - 1;
+                    yRnd = (r.NextDouble() * 2) - 1;
+                    s = (xRnd * xRnd) + (yRnd * yRnd);
+                }
+
+                xRnd = xRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+                yRnd = yRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+                value2 = xRnd * xRnd;
+
+                foreach (double key in istogramDict.Keys)
+                {
+                    double range = key + intervalsSize;
+                    if (range > maxValue) range = maxValue;
+                    if (value2 < range && value2 > key)
+                    {
+                        istogramDict[key] += 1;
+                        if (total < istogramDict[key])
+                        {
+                            total = istogramDict[key];
+                        }
+                        break;
+                    }
+                }
+            }
+            g.TranslateTransform(0, this.b.Height);
+            g.ScaleTransform(1, -1);
+
+            int idIstogram = 0;
+            int widthIstogram = (int)(this.b.Width / nintervals);
+            double lastKeyY = 0;
+
+            foreach (double key in istogramDict.Keys)
+            {
+                lastKeyY = key;
+                int newHeight = istogramDict[key] * this.b.Height / total;
+                int newX = (widthIstogram * idIstogram) + 10;
+                Rectangle isto = new Rectangle(newX, 0, widthIstogram, newHeight);
+                idIstogram++;
+
+                int nextWidthIstogram = (int)(widthIstogram * idIstogram * 1);
+
+                g.DrawRectangle(Pens.Green, isto);
+                g.FillRectangle(Brushes.Green, isto);
+
+            }
+
+            this.pictureBox1.Image = b;
+        }
+
+        private void setUpWindow()
+        {
+            this.b = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            this.g = Graphics.FromImage(b);
+            this.g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g.Clear(Color.White);
+
+            Rectangle window1 = new Rectangle(0, 0, this.b.Width - 1, this.b.Height - 1);
+            g.DrawRectangle(Pens.Black, window1);
+            numberOftrials = (int)trackBar1.Value;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.setUpWindow();
+            double minValue = -8;
+            double maxValue = 8;
+            double delta = maxValue - minValue;
+            double nintervals = 150;
+            double intervalsSize = delta / nintervals;
+
+            Dictionary<double, int> istogramDict = new Dictionary<double, int>();
+            double tempValue = minValue;
+            for (int i = 0; i <= nintervals; i++)
+            {
+                tempValue = minValue + (intervalsSize * i);
+                tempValue = Math.Round(tempValue, 2);
+                istogramDict[tempValue] = 0;
+            }
+
+            int total = 0;
+
+            for (int x = 0; x < this.numberOftrials; x++)
+            {
+                double xRnd = (r.NextDouble() * 2) - 1;
+                double value3 = 0;
+                double yRnd = (r.NextDouble() * 2) - 1;
+                double s = (xRnd * xRnd) + (yRnd * yRnd);
+
+                while (s < 0 || s > 1)
+                {
+                    xRnd = (r.NextDouble() * 2) - 1;
+                    yRnd = (r.NextDouble() * 2) - 1;
+                    s = (xRnd * xRnd) + (yRnd * yRnd);
+                }
+
+                xRnd = xRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+                yRnd = yRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+
+                value3 = xRnd / (yRnd * yRnd);
+
+                foreach (double key in istogramDict.Keys)
+                {
+                    double range = key + intervalsSize;
+                    if (range > maxValue) range = maxValue;
+                    if (value3 < range && value3 > key)
+                    {
+                        istogramDict[key] += 1;
+                        if (total < istogramDict[key])
+                        {
+                            total = istogramDict[key];
+                        }
+                        break;
+                    }
+                }
+            }
+            g.TranslateTransform(0, this.b.Height);
+            g.ScaleTransform(1, -1);
+
+            int idIstogram = 0;
+            int widthIstogram = (int)(this.b.Width / nintervals);
+            double lastKeyY = 0;
+
+            foreach (double key in istogramDict.Keys)
+            {
+                lastKeyY = key;
+                int newHeight = istogramDict[key] * this.b.Height / total;
+                int newX = (widthIstogram * idIstogram) + 10;
+                Rectangle isto = new Rectangle(newX, 0, widthIstogram, newHeight);
+                idIstogram++;
+
+                int nextWidthIstogram = (int)(widthIstogram * idIstogram * 1);
+
+                g.DrawRectangle(Pens.Green, isto);
+                g.FillRectangle(Brushes.Green, isto);
+
+            }
+            this.pictureBox1.Image = b;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.setUpWindow();
+
+            double minValue = 0;
+            double maxValue = 4;
+            double delta = maxValue - minValue;
+            double nintervals = 150;
+            double intervalsSize = delta / nintervals;
+
+            Dictionary<double, int> istogramDict = new Dictionary<double, int>();
+            double tempValue = minValue;
+            for (int i = 0; i <= nintervals; i++)
+            {
+                tempValue = minValue + (intervalsSize * i);
+                tempValue = Math.Round(tempValue, 2);
+                istogramDict[tempValue] = 0;
+            }
+
+            int total = 0;
+
+            for (int x = 0; x < this.numberOftrials; x++)
+            {
+                double xRnd = (r.NextDouble() * 2) - 1;
+                double value4 = 0;
+                double yRnd = (r.NextDouble() * 2) - 1;
+                double s = (xRnd * xRnd) + (yRnd * yRnd);
+
+                while (s < 0 || s > 1)
+                {
+                    xRnd = (r.NextDouble() * 2) - 1;
+                    yRnd = (r.NextDouble() * 2) - 1;
+                    s = (xRnd * xRnd) + (yRnd * yRnd);
+                }
+
+                xRnd = xRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+                yRnd = yRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+
+
+                value4 = (xRnd * xRnd) / (yRnd * yRnd);
+
+                foreach (double key in istogramDict.Keys)
+                {
+                    double range = key + intervalsSize;
+                    if (range > maxValue) range = maxValue;
+                    if (value4 < range && value4 > key)
+                    {
+                        istogramDict[key] += 1;
+                        if (total < istogramDict[key])
+                        {
+                            total = istogramDict[key];
+                        }
+                        break;
+                    }
+                }
+            }
+
+            g.TranslateTransform(0, this.b.Height);
+            g.ScaleTransform(1, -1);
+
+            int idIstogram = 0;
+            int widthIstogram = (int)(this.b.Width / nintervals);
+            double lastKeyY = 0;
+
+            foreach (double key in istogramDict.Keys)
+            {
+                lastKeyY = key;
+                int newHeight = istogramDict[key] * this.b.Height / total;
+                int newX = (widthIstogram * idIstogram) + 10;
+                Rectangle isto = new Rectangle(newX, 0, widthIstogram, newHeight);
+                idIstogram++;
+
+                int nextWidthIstogram = (int)(widthIstogram * idIstogram * 1);
+
+                g.DrawRectangle(Pens.Green, isto);
+                g.FillRectangle(Brushes.Green, isto);
+
+            }
+            this.pictureBox1.Image = b;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {    
+            this.setUpWindow();
+            double minValue = -10;
+            double maxValue = 10;
+            double delta = maxValue - minValue;
+            double nintervals = 150;
+            double intervalsSize = delta / nintervals;
+
+            Dictionary<double, int> istogramDict = new Dictionary<double, int>();
+            double tempValue = minValue;
+            for (int i = 0; i <= nintervals; i++)
+            {
+                tempValue = minValue + (intervalsSize * i);
+                tempValue = Math.Round(tempValue, 2);
+                istogramDict[tempValue] = 0;
+            }
+
+            int total = 0;
+
+            for (int x = 0; x < this.numberOftrials; x++)
+            {
+                double xRnd = (r.NextDouble() * 2) - 1;
+                double value5 = 0;
+                double yRnd = (r.NextDouble() * 2) - 1;
+                double s = (xRnd * xRnd) + (yRnd * yRnd);
+
+                while (s < 0 || s > 1)
+                {
+                    xRnd = (r.NextDouble() * 2) - 1;
+                    yRnd = (r.NextDouble() * 2) - 1;
+                    s = (xRnd * xRnd) + (yRnd * yRnd);
+                }
+
+                xRnd = xRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+                yRnd = yRnd * Math.Sqrt(-2 * Math.Log(s) / s);
+                value5 = xRnd / yRnd;
+
+                foreach (double key in istogramDict.Keys)
+                {
+                    double range = key + intervalsSize;
+                    if (range > maxValue) range = maxValue;
+                    if (value5 < range && value5 > key)
+                    {
+                        istogramDict[key] += 1;
+                        if (total < istogramDict[key])
+                        {
+                            total = istogramDict[key];
+                        }
+                        break;
+                    }
+                }
+            }
+            g.TranslateTransform(0, this.b.Height);
+            g.ScaleTransform(1, -1);
+
+            int idIstogram = 0;
+            int widthIstogram = (int)(this.b.Width / nintervals);
+            double lastKeyY = 0;
+
+            foreach (double key in istogramDict.Keys)
+            {
+                lastKeyY = key;
+                int newHeight = istogramDict[key] * this.b.Height / total;
+                int newX = (widthIstogram * idIstogram) + 1;
+                Rectangle isto = new Rectangle(newX, 0, widthIstogram, newHeight);
+                idIstogram++;
+
+                int nextWidthIstogram = (int)(widthIstogram * idIstogram * 1);
+
+                g.DrawRectangle(Pens.Green, isto);
+                g.FillRectangle(Brushes.Green, isto);
+
+            }
+            this.pictureBox1.Image = b;
+        }
+    }
+}
