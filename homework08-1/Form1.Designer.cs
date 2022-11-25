5374b9ae4727decd2df9f7fffe5028513741bc15
rocco1414 <114834587+rocco1414@users.noreply.github.com>
2022-11-25 09:42:29 +0100

    Add files via upload

---
+++ homework08-1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace homework08_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(148, 12);
+            this.trackBar1.Maximum = 10000;
+            this.trackBar1.Minimum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "100";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(470, 462);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(488, 74);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(466, 462);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(960, 74);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(404, 462);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 462);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(488, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(466, 462);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(960, 74);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(404, 459);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(626, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Empirical distribution of X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1111, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Empirical distribution of Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Number of throws: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 545);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ homework08-1/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework08_1
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        Pen pen = new Pen(Color.Green);
+        Bitmap b;
+        Graphics g;
+
+
+        Bitmap b2;
+        Graphics g2;
+
+        Bitmap b3;
+        Graphics g3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int FromXRealToXVirtual(double X, double minX, double maxX, int Left, int W)
+        {
+            if (maxX - minX == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (int)(Left + W * (X - minX) / (maxX - minX));
+            }
+        }
+
+        private int FromYRealToYVirtual(double Y, double minY, double maxY, int Top, int H)
+        {
+            if (maxY - minY == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (int)(Top + H - H * (Y - minY) / (maxY - minY));
+            }
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.b = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            this.g = Graphics.FromImage(b);
+
+            this.g.Clear(Color.White);
+
+            Rectangle window = new Rectangle(0, 0, this.b.Width - 1, this.b.Height - 1);
+            g.DrawRectangle(Pens.Black, window);
+
+            int nPoints = (int)trackBar1.Value;
+
+            double minX = -100;
+            double maxX = 100;
+            double minY = -100;
+            double maxY = 100;
+
+            List<double> X = new List<double>();
+            List<double> Y = new List<double>();
+
+
+            for (int i = 0; i < nPoints; i++)
+            {
+                double radius = r.NextDouble() * 100;
+                double angle = r.Next(0, 360);
+                double xCoord = radius * Math.Cos(angle);
+                double yCoord = radius * Math.Sin(angle);
+
+                int xDevice = FromXRealToXVirtual(xCoord, minX, maxX, window.Left, window.Width);
+                int yDevice = FromYRealToYVirtual(yCoord, minY, maxY, window.Top, window.Height);
+
+                Rectangle rectangle = new Rectangle(xDevice, yDevice, 1, 1);
+                g.DrawRectangle(pen, rectangle);
+                g.FillRectangle(Brushes.Black, rectangle);
+
+                X.Add(xDevice);
+                Y.Add(yDevice);
+
+            }
+
+            this.pictureBox1.Image = b;
+
+            this.b2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            this.g2 = Graphics.FromImage(b2);
+            this.g2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g2.Clear(Color.White);
+
+            Rectangle Window2 = new Rectangle(0, 0, this.b2.Width - 1, this.b2.Height - 1);
+            g2.DrawRectangle(Pens.Black, Window2);
+
+            double minValueX = X.Min();
+            double maxValueX = X.Max();
+            double delta = maxValueX - minValueX;
+            double nIntervals = 15;
+            double sizeIntervalX = delta / nIntervals;
+
+            Dictionary<double, int> XHinstogram = new Dictionary<double, int>();
+
+            double tempValue = minValueX;
+            for (int i = 0; i < nIntervals; i++)
+            {
+                XHinstogram[tempValue] = 0;
+                tempValue = tempValue + sizeIntervalX;
+            }
+
+            int tot = 0;
+
+            foreach (double value in X)
+            {
+                foreach (double key in XHinstogram.Keys)
+                {
+                    if (value < key + sizeIntervalX)
+                    {
+                        XHinstogram[key] += 1;
+                        if (tot < XHinstogram[key])
+                        {
+                            tot = XHinstogram[key];
+                        }
+                        break;
+                    }
+                }
+            }
+
+            g2.TranslateTransform(0, this.b2.Height);
+            g2.ScaleTransform(1, -1);
+
+            int idHistogram = 0;
+            int widthHistogram = (int)(this.b2.Width / nIntervals);
+            double lastKey = 0;
+
+            foreach (double key in XHinstogram.Keys)
+            {
+                lastKey = key;
+                int newHeight = XHinstogram[key] * this.b2.Height / tot;
+                int newX = (widthHistogram * idHistogram) + 1;
+                Rectangle histogram = new Rectangle(newX, 0, widthHistogram, newHeight);
+                idHistogram++;
+
+
+                g2.DrawRectangle(Pens.Black, histogram);
+                g2.FillRectangle(Brushes.Green, histogram);
+            }
+
+
+            this.pictureBox2.Image = b2;
+
+
+            this.b3 = new Bitmap(this.pictureBox3.Width, this.pictureBox3.Height);
+            this.g3 = Graphics.FromImage(b3);
+            this.g3.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.g3.Clear(Color.White);
+
+            Rectangle VirtualWindow3 = new Rectangle(0, 0, this.b3.Width - 1, this.b3.Height - 1);
+            g3.DrawRectangle(Pens.Black, VirtualWindow3);
+
+            double minValueY = Y.Min();
+            double maxValueY = Y.Max();
+            double deltaY = maxValueY - minValueY;
+            double sizeIntervalY = deltaY / nIntervals;
+
+            Dictionary<double, int> YHistogram = new Dictionary<double, int>();
+
+            double tempValueY = minValueY;
+            for (int i = 0; i < nIntervals; i++)
+            {
+                YHistogram[tempValueY] = 0;
+                tempValueY = tempValueY + sizeIntervalY;
+            }
+
+            int totalY = 0;
+
+            foreach (double value in Y)
+            {
+                foreach (double key in YHistogram.Keys)
+                {
+                    if (value < key + sizeIntervalY)
+                    {
+                        YHistogram[key] += 1;
+                        if (totalY < YHistogram[key])
+                        {
+                            totalY = YHistogram[key];
+                        }
+                        break;
+                    }
+                }
+            }
+
+            g3.TranslateTransform(0, this.b3.Height);
+            g3.ScaleTransform(1, -1);
+
+            idHistogram = 0;
+            int widthHistogramY = (int)(this.b3.Width / nIntervals);
+            double lastKeyY = 0;
+
+            foreach (double key in YHistogram.Keys)
+            {
+                lastKeyY = key;
+                int newHeight = YHistogram[key] * this.b3.Height / totalY;
+                int newX = (widthHistogramY * idHistogram) + 1;
+                Rectangle isto = new Rectangle(newX, 0, widthHistogramY, newHeight);
+                idHistogram++;
+
+                g3.DrawRectangle(Pens.Black, isto);
+                g3.FillRectangle(Brushes.Green, isto);
+
+
+            }
+
+
+            this.pictureBox3.Image = b3;
+
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+        }
+    }
+}
